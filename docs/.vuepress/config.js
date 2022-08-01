9075194563ae5611fdefd5deb21ccdbd4da5d88b
KuaiZiWa <kuaiziwa@qq.com>
2022-08-01 11:09:08 +0800

    add gtag to head

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,5 @@
 const { path } = require('@vuepress/utils')
 const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
-const  googleAnalyticsPlugin = require('@vuepress/plugin-google-analytics')
 
 const { defaultTheme } = require('@vuepress/theme-default')
 
@@ -8,15 +7,15 @@ module.exports = {
   base: 'mongodb-gui-tools',
   title: 'Top MongoDB GUI Tools',
   description: 'Features comparison of the following five MongoDB GUI tools, Compass, Navicat, NoSQLBooster, NoSQLManager and Studio 3T.',
-  
+
   plugins: [
     registerComponentsPlugin({
       componentsDir: path.resolve(__dirname, './components'),
     }),
-    
-    googleAnalyticsPlugin({
-      id: 'G-ZS6PPJJSPF'
-    }),
+
+    // googleAnalyticsPlugin({
+    //   id: 'G-ZS6PPJJSPF'
+    // }),
 
   ],
   theme: defaultTheme({
@@ -26,11 +25,24 @@ module.exports = {
   }),
 
   head: [
-     ['link', { rel: 'icon', href: `/favicon.ico` }],
-     //['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' }],
+    ['link', { rel: 'icon', href: `/favicon.ico` }],
+
+    ['script', {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-ZS6PPJJSPF'
+    }],
+    ['script', {}, `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-ZS6PPJJSPF');
+  `],
+
+    //['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' }],
 
     //  ['link', { rel: 'stylesheet', href: 'http://fonts.cdnfonts.com/css/open-sans' }],
     //  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>, 
-     //<link href="http://fonts.cdnfonts.com/css/open-sans" rel="stylesheet">
+    //<link href="http://fonts.cdnfonts.com/css/open-sans" rel="stylesheet">
   ],
 }
