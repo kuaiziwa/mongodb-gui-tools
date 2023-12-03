4a56b0417a2fa7e6eda25bf0b6fd2f2489498644
KuaiZiWa <kuaiziwa@qq.com>
2023-12-04 07:18:04 +0800

    2023.12 release

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ const { defaultTheme } = require('@vuepress/theme-default')
 module.exports = {
   // base: '/mongodb-gui-tools/',
   title: 'Top MongoDB GUI Tools',
-  description: 'Feature Comparison for top MongoDB GUIs (Nov 2023)',
+  description: 'Feature Comparison for top MongoDB GUIs (Dec 2023)',
   pagePatterns: ['**/*.md', '!**/README.md', '!.vuepress', '!node_modules'],
 
 
