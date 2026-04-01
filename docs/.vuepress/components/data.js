173a2a6008c84c217e33f922d329321920aa0edc
KuaiZiWa <kuaiziwa@qq.com>
2026-04-02 07:54:20 +0800

    chore(version): 更新MongoDB GUI工具版本和文档日期
    - 将Studio 3T、NoSQLBooster、NoSQLManager等工具版本更新至2026年3月到6月的新版本
    - 更新文档首页和配置中的发布日期为2026年4月
    - 修改package.json版本号为1.0.17以反映更新
    - 保持工具功能描述和链接不变，仅更新时间点和版本信息

--- docs/.vuepress/components/data.js
+++ docs/.vuepress/components/data.js
@@ -20,7 +20,7 @@ export default {
                 name: "Basic and Connection",//
                 rows: [
                                //studio3t                       //nosqlbooster                compass                       navicat                       sqlmanager
-                    ["Release", tp.ver("2026.4.0","2026-02-25"),tp.ver("10.1.3","2026-02-15"), tp.ver("1.49.2","2026-02-25"),tp.ver("17.3.9","2026-02-03"),tp.ver("7.8.1","2026-01-06")],
+                    ["Release", tp.ver("2026.6.0","2026-03-24"),tp.ver("10.1.5","2026-03-30"), tp.ver("1.49.4","2026-03-11"),tp.ver("17.3.9","2026-02-03"),tp.ver("7.9.2","2026-03-27")],
                     [{name:"Cost",title:`- Free means that a free version with reduced features is available.\n- Price refers to the price of the most expensive full-featured version.`}, "free-$699/year", "free-$239/license", "free", "$449/license",  "free-$125/license"], 
                     ["Main Interface", 
                          tp.screen("Studio 3T", "https://studio3t.com/wp-content/uploads/2022/03/Studio_3T_Screenshot_Win_Dark.png"),
